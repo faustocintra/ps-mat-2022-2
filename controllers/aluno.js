11e940f516005ade028571d3a12083da75aa8feb
Thales Lima <thales.lima13@hotmail.com>
2022-08-26 14:10:26 -0300

    (26/08) adicionadas corretamente as verificações do retrieveOne e update no arquivos alunos da pasta controller

--- controllers/aluno.js
+++ controllers/aluno.js
@@ -31,6 +31,15 @@ controller.retrieve = async(req, res) => {
 controller.retrieveOne = async(req, res) => {
     try{
         const result = await Aluno.findByPk(req.params.id);
+
+        if(result) {
+            // HTTP 200: OK (implícito)
+            res.send(result)
+        }
+        else {
+            // HTTP 404: Not found  
+            res.status(404).end()
+        }
     }
     catch(error){
         console.error(error);
@@ -45,10 +54,13 @@ controller.update = async(req, res) => {
             req.body,
             {where: {id: req.body.id}}
             );
-        if(response){
+        // console.log("======>", {response})
+
+        if(response[0] > 0) {  // Encontrou e atualizou
+            // HTTP 204: No content
             res.status(204).end()
         }
-        else{
+        else {  // Não encontrou (e não atualizou)
             res.status(404).end()
         }
     }
