3f2c687465398ea19c914e1c9f15143036538254
Thales Gabriel <71100548+Thales-Gabriel42@users.noreply.github.com>
2022-08-26 09:57:08 -0300

    19/08/2022
    Configuração de conexão com o banco de dados

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+
+async function connect(){
+
+    const sequelize = new Sequelize(
+        process.env.APP_DB_NAME, 
+        process.env.APP_DB_USER, 
+        process.env.APP_DB_PASS, {
+            host: process.env.APP_DB_HOST,
+            dialect: 'mysql'
+            }
+        );
+
+    try {
+        //Sincroniza a aplicação com o BD
+        await sequelize.authenticate(); 
+        console.log('Connection has been established successfully.');
+        return sequelize;
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        process.exit(1); // Encerrar o servidor
+    }
+}
+
+module.exports = connect();
