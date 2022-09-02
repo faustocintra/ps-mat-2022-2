e9a6a748030d9676dda72b2386ba3c70b961acf7
Leomendferre <leo.mend@hotmail.com>
2022-09-02 15:26:33 +0000

    (02/09) Trabalho 1 - Professor

---
+++ routes/professor.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/professor')
+
+router.post('/', controller.create)
+router.get('/', controller.retrieve)
+// :id é uma parte variavel de URI que será interpretada como um parâmetro cahamdo ID
+router.get('/:id', controller.retrieveOne)
+router.patch('/:id', controller.update)
+router.delete('/:id', controller.delete)
+
+module.exports = router
