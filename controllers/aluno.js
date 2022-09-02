faf77ce2f901e24ed40d9edc7dec946e8df60594
Gustavo Silva <gustavosilva.gs2001@hotmail.com>
2022-09-02 14:01:47 +0000

    (02/09)Término do primeiro CRUD

--- controllers/aluno.js
+++ controllers/aluno.js
@@ -57,12 +57,33 @@ controller.update = async(req, res) => {
     try {
         const response = await Aluno.update(
             req.body,
-            { where: { id: req.body.id }} 
+            { where: { id: req.params.id }} 
         )
 
         console.log("======", {response})
 
         if(response[0] > 0) { // Encontrou e atualizou
+            // HTTP 204: No content
+            res.status(204).end()
+        } else { // Não encontrou (e não atualizou)
+            res.status(404).end()
+        }
+    }
+    catch(error) {
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+controller.delete = async (req, res) => {
+    try {
+        const response = await Aluno.destroy(
+            { where: { id: req.params.id }} 
+        )
+
+        // console.log("======", {response})
+
+        if(response) { // Encontrou e atualizou
             // HTTP 204: No content
             res.status(204).end()
         } else { // Não encontrou (e não atualizou)
--- routes/aluno.js
+++ routes/aluno.js
@@ -6,6 +6,7 @@ router.post('/', controller.create)
 router.get('/', controller.retrieve)
 // :id é uma parte variável da URI 
 router.get('/:id', controller.retrieveOne)
-router.put('/', controller.update)
+router.patch('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
 module.exports = router
