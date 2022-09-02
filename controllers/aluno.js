5a981ddf57b359b6e9a5c2401693437c14aa77af
Thales Gabriel <thales.lima13@hotmail.com>
2022-09-02 14:01:52 +0000

    (02/09) Término do primeiro CRUD

--- controllers/aluno.js
+++ controllers/aluno.js
@@ -6,7 +6,7 @@ controller.create = async(req, res) => {
     try{
         await Aluno.create(req.body)
         // HTTP 200 = OK (Implicito)
-        res.send(result);
+        res.status(201).end();
     }
     catch(error){
         console.error(error);
@@ -52,11 +52,34 @@ controller.update = async(req, res) => {
     try{
         const response = await Aluno.update(
             req.body,
-            {where: {id: req.body.id}}
+            // {where: {id: req.body.id}}
+            {where: {id: req.params.id}}
             );
         // console.log("======>", {response})
 
         if(response[0] > 0) {  // Encontrou e atualizou
+            // HTTP 204: No content
+            res.status(204).end()
+        }
+        else {  // Não encontrou (e não atualizou)
+            res.status(404).end()
+        }
+    }
+    catch(error){
+        console.error(error);
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error);
+    }
+}
+
+controller.delete = async(req, res) => {
+    try{
+        const response = await Aluno.destroy(
+            {where: {id: req.params.id}}
+            );
+        console.log("======>", {response})
+
+        if(response) {  // Encontrou e atualizou
             // HTTP 204: No content
             res.status(204).end()
         }
--- routes/aluno.js
+++ routes/aluno.js
@@ -5,6 +5,8 @@ const controller = require('../controllers/aluno');
 router.post('/', controller.create);
 router.get('/', controller.retrieve);
 router.get('/:id', controller.retrieveOne);
-router.put('/', controller.update)
+// router.put('/', controller.update)
+router.patch('/:id', controller.update) // Pegar o ID na rota
+router.delete('/:id', controller.delete) // Pegar o ID na rota
 
 module.exports = router;
