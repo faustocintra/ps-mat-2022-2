e5b86d228ea320f0bf260657ee0112abf0cb0438
Bruno Torquato <brunohenriquecosta2014@gmail.com>
2022-09-09 15:30:32 +0000

    (09/09) - Migatrion no sequelize, implement JWT

---
+++ models/usuario.js
@@ -0,0 +1,123 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/db');
+
+const model = db.define('Usuario', {
+// Model attributes are defined here
+id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nome: { 
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true
+  },
+  hash_senha: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  admin: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  data_nasc: {
+    type:DataTypes.DATE,
+    allowNull: false
+  }
+
+}, {
+    tableName: 'usuarios'
+});
+
+// Cria a tabela no banco de dados, caso ainda não exista
+model.sync()
+
+module.exports = model
+
+
+
+// const { DataTypes } = require('sequelize');
+// const db = require('../config/db');
+
+
+// module.exports = function() {
+
+// const model = db.define('Aluno', {
+//   // Model attributes are defined here
+//   id: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//     primaryKey: true,
+//     autoIncrement: true
+//   },
+//   nome: {
+//     type: DataTypes.STRING(100),
+//     // allowNull defaults to true
+//     allowNull: false
+//   },
+//   data_nascimento: {
+//     type: DataTypes.DATEONLY,
+//     allowNull: false
+//   },
+//   doc_identidade: {
+//     type: DataTypes.STRING(20),
+//     allowNull: false
+//   },
+//   cpf: {
+//     type: DataTypes.STRING(100),
+//     allowNull: false
+//   },
+//   logradouro: {
+//     type: DataTypes.STRING(100),
+//     allowNull: false
+//   },
+//   num_imovel: {
+//     type: DataTypes.STRING(10),
+//     allowNull: false
+//   },
+//   complemento: {
+//     type: DataTypes.STRING(50),
+//     allowNull: true
+//   },
+//   bairro: {
+//     type: DataTypes.STRING(50),
+//     allowNull: false
+//   },
+//   uf: {
+//     type: DataTypes.CHAR(2),
+//     allowNull: false
+//   },
+//   telefone: {
+//     type: DataTypes.STRING(20),
+//     allowNull: false
+//   },
+//   email: {
+//     type: DataTypes.STRING(50),
+//     allowNull: false
+//   },
+//   telefone: {
+//     type: DataTypes.STRING(20),
+//     allowNull: false
+//   },
+//   turma: {
+//     type: DataTypes.CHAR(5),
+//     allowNull: false
+//   },
+  
+
+// }, {
+//   tableName: 'alunos'
+// });
+
+// model.async()
+
+// return model
+
+// }
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const controller = require('../controllers/aluno')
+const controller = require('../controllers/usuario')
 
 router.post('/', controller.create)
 router.get('/', controller.retrieve)
@@ -9,5 +9,7 @@ router.get('/', controller.retrieve)
 router.get('/:id', controller.retrieveOne)
 router.patch('/:id', controller.update)
 router.delete('/:id', controller.delete)
+
+router.post('/login', controller.login)
 
 module.exports = router
